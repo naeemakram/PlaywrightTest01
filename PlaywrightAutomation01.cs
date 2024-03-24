9023ea25d4fa75493e54dbe68de3cc8f5f943ddc
Naeem Malik <naeem.akram.malik@gmail.com>
2024-03-24 22:24:16 +0500

    Learning Playwright

---
+++ PlaywrightAutomation01.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+namespace PlaywrightTest01
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class PlaywrightAutomation01: PageTest
+    {
+         [SetUp]
+        public async Task Setup()
+        {
+            await Context.Tracing.StartAsync(new()
+            {
+                Title = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
+                Screenshots = true,
+                Snapshots = true,
+                Sources = true
+            });
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            Console.WriteLine($"Directory: {TestContext.CurrentContext.WorkDirectory}");
+            // This will produce e.g.:
+            // bin/Debug/net8.0/playwright-traces/PlaywrightTests.Tests.Test1.zip
+            await Context.Tracing.StopAsync(new()
+            {
+                Path = Path.Combine(
+                    TestContext.CurrentContext.WorkDirectory,
+                    "playwright-traces",
+                    $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip"
+                )
+            });
+        }
+
+        [Test]
+        public async Task StartingPlaywright()
+        {
+            await Page.GotoAsync("https://playwright.dev");
+
+            // Expect a title "to contain" a substring.
+            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+            // create a locator
+            var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+
+            // Expect an attribute "to be strictly equal" to the value.
+            await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+            // Click the get started link.
+            await getStarted.ClickAsync();
+
+            // Expects the URL to contain intro.
+            await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+            
+        }
+    }
+}
